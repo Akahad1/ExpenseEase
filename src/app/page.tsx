c5f6568b3a77e482c6df620ab7e94b38c717f5a6
Akahad1 <aksahad234567@gmail.com>
2025-01-10 11:25:16 +0600

    add redux

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
 import ExpenseTracker from "@/component/ExpenseTracker/ExpenseTracker";
 import FirstTime from "@/component/FirstTime/FirstTime";
+import "./globals.css";
 
 export default function Home() {
   return (
     <div>
       <FirstTime></FirstTime>
-      <ExpenseTracker></ExpenseTracker>
+
+      <div className="container">
+        <div className="col-4">
+          {" "}
+          <ExpenseTracker></ExpenseTracker>
+        </div>
+        <div className="col-8 color">Second Div</div>
+      </div>
     </div>
   );
 }
---
+++ src/component/GobalRedux/Features/Api/baseApi.ts
@@ -0,0 +1,15 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+// Define the baseQuery using fetchBaseQuery
+const baseQuery = fetchBaseQuery({
+  baseUrl: "http://localhost:5000",
+  //   credentials: "include",
+});
+
+// Create the base API slice using the enhanced baseQueryWithRefreshToken
+export const baseApi = createApi({
+  reducerPath: "baseApi",
+  baseQuery: baseQuery,
+  tagTypes: [],
+  endpoints: () => ({}),
+});
